18dfafb9e6fb3ae6787796c3b2ad54124a1f7312
sam Matthews <sam.matthews.developer@gmail.com>
2020-05-08 18:14:49 +1200

    Initial commit.

---
+++ bin/download.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+# download.sh
+# Sam Matthews
+# 22nd September 2019
+
+# Description
+# ============
+# Script will download US Stock data based on CFG list.
+
+# Modifications:
+# Moved Sleep to 12
+# Moved DAT to seperate directories for DAILY and WEEKLY
+# New function for weekly data
+
+# Setup
+
+# API: curl https://www.alphavantage.co/query\?function\=TIME_SERIES_DAILY\&symbol\=AAPL\&apikey\=WHWGAETT94IZAL0B\&datatype\=csv --output AAPL.csv
+# Parameters
+
+DEV_HOME="${HOME}/dev/projects/alphavantage"
+CFG=${DEV_HOME}/cfg
+LOG=${DEV_HOME}/log
+DAT=${DEV_HOME}/dat
+BIN=${DEV_HOME}/bin
+DAT_DAILY=${DAT}/daily
+DAT_WEEKLY=${DAT}/weekly
+
+BASEURL="https://www.alphavantage.co/query?function"
+DAILY_FUNCTION="TIME_SERIES_DAILY"
+WEEKLY_FUNCTION="TIME_SERIES_WEEKLY"
+SMAFUNCTION="SMA"
+SMAINTERVAL="daily"
+SERIESTYPE="close"
+TIMEPERIOD="300"
+OUTPUTSIZE="full"
+# APIKEY="WHWGAETT94IZAL0B"
+APIKEY="CQ1QMHUNGM68QOWG"
+DATATYPE="csv"
+
+CURR_DATE=`date +%Y%m%d`
+CURR_TIME=`date +%Y%m%d-%H:%M:%S`
+SLEEP=0
+SCRIPT_LOGFILE="${LOG}/download-${CURR_DATE}.log"
+
+get_daily_price () {
+
+  for SYMBOL in $(cat ${CFG}/symbol.cfg)
+  do
+    echo "${CURR_TIME}: downloading day data for ${SYMBOL}." >> ${SCRIPT_LOGFILE} 2>&1
+    LOG_FILE="${LOG}/${SYMBOL}.log"
+    OUTPUT_FILE="${DAT_DAILY}/${SYMBOL}.${DATATYPE}"
+    URL="${BASEURL}=${DAILY_FUNCTION}&symbol=${SYMBOL}&outputsize=${OUTPUTSIZE}&apikey=${APIKEY}&datatype=${DATATYPE}"
+    curl ${URL} --output ${OUTPUT_FILE} > ${LOG_FILE} 2>&1
+    sleep ${SLEEP}
+  done
+}
+
+get_weekly_price () {
+
+  for SYMBOL in $(cat ${CFG}/symbol.cfg)
+  do
+    echo "${CURR_TIME}: downloading week data for ${SYMBOL}." >> ${SCRIPT_LOGFILE} 2>&1
+
+    LOG_FILE="${LOG}/${SYMBOL}-WEEKLY.log"
+    OUTPUT_FILE="${DAT_WEEKLY}/${SYMBOL}.${DATATYPE}"
+    URL="${BASEURL}=${WEEKLY_FUNCTION}&symbol=${SYMBOL}&outputsize=${OUTPUTSIZE}&apikey=${APIKEY}&datatype=${DATATYPE}"
+    curl ${URL} --output ${OUTPUT_FILE} > ${LOG_FILE} 2>&1
+    sleep ${SLEEP}
+  done
+}
+
+get_sma_daily () {
+
+  # set parameter
+  SMATIMEPERIOD=$1
+
+  #check if DAT directory exists. Create it if it does not.
+  if [ ! -d ${DAT}/SMA${SMATIMEPERIOD} ]; then
+    mkdir -p ${DAT}/SMA${SMATIMEPERIOD}
+  fi
+
+  # LOOP, to download data
+  for SYMBOL in $(cat ${CFG}/symbol.cfg)
+  do
+    echo "${CURR_TIME}: downloading SMA ${SMATIMEPERIOD} day data for ${SYMBOL}." \
+      >> ${SCRIPT_LOGFILE} 2>&1
+
+    LOG_FILE="${LOG}/${SYMBOL}-SMA-${SMATIMEPERIOD}.log"
+    OUTPUT_FILE="${DAT}/SMA${SMATIMEPERIOD}/${SYMBOL}.${DATATYPE}"
+
+    URL="${BASEURL}=${SMAFUNCTION}&symbol=${SYMBOL}&interval=${SMAINTERVAL}&time_period=${SMATIMEPERIOD}&series_type=${SERIESTYPE}&apikey=${APIKEY}&datatype=${DATATYPE}"
+
+    curl ${URL} --output ${OUTPUT_FILE} > ${LOG_FILE} 2>&1
+    sleep ${SLEEP}
+  done
+}
+
+#
+# MAIN
+#
+
+get_daily_price
+# get_weekly_price
+get_sma_daily 6
+# get_sma_daily 12
+
+# TAR Files
+
+# tar xvf ${DAT} stock-data-${CURR_DATE}.tar
